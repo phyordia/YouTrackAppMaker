69e2923ee131dd8c2b3a2a47e367cf41ca31570f
Ricardo Neves <rmneves@kpmg.com>
2020-10-19 20:24:40 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # YouTrackAppMaker
 Make a .App for JetBrains YouTracker
+
+
+To build an app run: 
+
+```bash
+./make_app.sh
+```
+
+If you need/want to re-create the icon set, uncomment the respective lines in the `make_app.sh` script.
--- make_app.sh
+++ make_app.sh
@@ -4,25 +4,39 @@ PORT=8880
 BASEURL="http://localhost"
 BASEDIR=$HOME/.youtrack
 
+# Install Python dependencies
+echo "Installing Python dependencies"
+pip install py2app rumps
+
 
 # Download
-wget https://download.jetbrains.com/charisma/youtrack-2020.3.12000.zip
+echo "Downloading YopuTrack Zip"
+if ls youtrack?*.zip 1> /dev/null 2>&1; then
+    echo "Zip file exists - not downloading"
+else
+    echo "Zip file do not exist - downloading"
+    wget https://download.jetbrains.com/charisma/youtrack-2020.3.12000.zip
+fi
+
 
 # Unzip
+echo "Extacting Zip file"
 find . -iname "youtrack*.zip" -exec unzip {}  \;
 find . -type d -iname "youtrack*" -maxdepth 1 -exec mv '{}' youtrack \;
 
-# Create Icon
-mkdir images
-./svg2icns.sh youtrack/internal/error_pages/logos/youtrack/youtrack.svg images/youtrack
+# Create Icon - Requires Inkscape installed. This should be uncommented for recreating icon set (images/youtrack.icns)
+#mkdir images
+#./svg2icns.sh youtrack/internal/error_pages/logos/youtrack/youtrack.svg images/youtrack
 
 
 # Build App bundle
+echo "Building App bundle"
 rm -rf build dist 
 python setup.py py2app --resources youtrack
 mv dist/youtrack_launcher.app .
 
-
+# Configuring YouTrack Settings
+echo "Configuring YouTrack Settings"
 ./youtrack_launcher.app/Contents/Resources/youtrack/bin/youtrack.sh configure \
         --listen-port=${PORT} \
         --data-dir="${BASEDIR}/data" \
@@ -31,4 +45,7 @@ mv dist/youtrack_launcher.app .
         --base-url="${BASEURL}:${PORT}"
 
 #cleanup
+echo 'Cleaning Up'
 rm -rf youtrack/ build/ dist/
+
+echo 'Done'
