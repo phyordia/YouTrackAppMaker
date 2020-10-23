053bde76a70ff29d8a48f2ea851c9f97845637df
Ricardo Neves <rmneves@kpmg.com>
2020-10-23 11:58:11 -0400

    adding Open menu item

--- YouTrackerLauncher_template.py
+++ YouTrackerLauncher_template.py
@@ -3,6 +3,8 @@ import rumps
 import subprocess
 import sys
 import os
+import re
+import webbrowser
 
 rumps.debug_mode(True)
 
@@ -11,33 +13,46 @@ class App(rumps.App):
         super(App, self).__init__("YT")
         self.basedir = "{{BASEDIR}}"
         self.icon = os.path.join(self.basedir, "src/apps/youtrack/web/favicon.ico")
-        self.menu = ["Toggle"]
+        self.menu = ["Open", "Toggle"]
         self.toggle = self.menu['Toggle']
         self.toggle.title = "Start"
         self.quit_button=None
+        self.menu['Open'].set_callback(None)
 
+
+    def open_window(self, _):
+        rumps.notification("YouTrack", "Opening", "", sound=False)
+        webbrowser.open_new_tab(self.url)
 
     @rumps.clicked("Toggle")
     def prefs(self, sender):
         toggle = self.menu['Toggle']
         if toggle.title == "Start":
-            rumps.notification("YouTrack", "Launching YouTrack", "This may take a few seconds...")
-            subprocess.run([ os.path.join(self.basedir, "src/bin/youtrack.sh"), "start"],  stdout=sys.stdout, stderr=sys.stderr)
-            rumps.notification("YouTrack", "Started", "")
+            rumps.notification("YouTrack", "Launching YouTrack", "This may take a few seconds...", sound=False)
+            proc_output = subprocess.run([ os.path.join(self.basedir, "src/bin/youtrack.sh"), "start"], capture_output=True)
+            re_res = re.search(".*available on .*\[(http.*)\].*",proc_output.stdout.decode('utf-8'))
+            if re_res:
+                self.url = re_res.group(1)
+            
             toggle.title = "Stop"
+            self.menu['Open'].set_callback(self.open_window)
+            rumps.notification("YouTrack", "Started", "", sound=False)
+            
         
         else:
-            rumps.notification("YouTrack", "Stopping YouTrack", "This may take a few seconds...")
+            rumps.notification("YouTrack", "Stopping YouTrack", "This may take a few seconds...", sound=False)
             subprocess.run([ os.path.join(self.basedir, "src/bin/youtrack.sh"), "stop"],  stdout=sys.stdout, stderr=sys.stderr)
-            rumps.notification("YouTrack", "Stopped", "")
             toggle.title = "Start"
+            self.menu['Open'].set_callback(None)
+            rumps.notification("YouTrack", "Stopped", "", sound=False)
+            
 
 
     @rumps.clicked('Quit')
     def clean_up_before_quit(self, _):
-        rumps.notification("YouTrack", "Stopping YouTrack", "This may take a few seconds...")
+        rumps.notification("YouTrack", "Stopping YouTrack", "This may take a few seconds...", sound=False)
         subprocess.run([ os.path.join(self.basedir, "src/bin/youtrack.sh"), "stop"],  stdout=sys.stdout, stderr=sys.stderr)
-        rumps.notification("YouTrack", "Stopped", "")
+        rumps.notification("YouTrack", "Stopped", "", sound=False)
         rumps.quit_application()
 
 
